968f350017b9524522821a42de64cb4e43d0c797
MariammkGithub <102911841+MariammkGithub@users.noreply.github.com>
2022-07-04 16:38:23 +0530

    Add files via upload

---
+++ rg form.js
@@ -0,0 +1,23 @@
+//function() {
+   // let x= document.getelementbyid('sss').value.Style.bg='blue'
+   // console.log('x')
+   
+
+function onclickColor() {
+
+
+    document.getElementById("sss").style.background="blue"
+} 
+
+function changebgcolor() {
+
+
+    document.getElementById("zzz").style.background="green"
+}
+
+function changecolor() {
+
+
+    document.getElementById("ttt").style.background="pink"
+} 
+
